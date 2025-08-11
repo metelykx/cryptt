6628f4b4f3ce3c0a128e33c6885e3974619b6e42
metelykx <ivaschenkoodenys@gmail.com>
2025-08-11 11:06:22 +0300

    buttonView

--- Cryptt/ButtonView.swift
+++ Cryptt/ButtonView.swift
@@ -7,4 +7,22 @@
 
 import SwiftUI
 
-
+struct ButtonView: View {
+    
+    let size: CGSize
+    var body: some View {
+        Button {
+            print("Sign Up tapped")
+        } label: {
+            Text("Sign Up")
+                .font(.headline)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, size.height * 0.03)
+         
+        }
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(12)
+        .padding(.horizontal, size.width * 0.2)
+        .padding(.top, size.height * 0.03)
+    }
+}
