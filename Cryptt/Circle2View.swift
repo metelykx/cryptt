a790255f1de50e20e7ff6f320f72dc986a66ebb7
metelykx <ivaschenkoodenys@gmail.com>
2025-08-11 11:02:46 +0300

    new struct

--- Cryptt/Circle2View.swift
+++ Cryptt/Circle2View.swift
@@ -6,3 +6,17 @@
 //
 
 import SwiftUI
+
+struct Circle2View: View {
+    let size: CGSize
+    var body: some View {
+        Circle()
+            .fill(Color.gray.opacity(0.2))
+            .frame(width: size.width / 2, height: size.height / 2)
+            .rotationEffect(.init(degrees: 45))
+            .offset(
+                x: size.width / 1.6,
+                y: size.height / 1.3
+            )
+    }
+}
