aa3e20d6675760755ba5440afede0ba522c751ec
metelykx <ivaschenkoodenys@gmail.com>
2025-08-11 11:00:09 +0300

    redesign header

--- Cryptt/HeaderView.swift
+++ Cryptt/HeaderView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct HeaderView: View {
+    let sizes: CGSize
+    @Environment(\.colorScheme) private var colorScheme
+    
+   
     var body: some View {
-        Text("deded")
+        HStack {
+            Text("Welcome to Crypt")
+                .font(.title)
+                .bold()
+                .fontWidth(.compressed)
+                .foregroundStyle(textColor)
+            
+            Image(systemName: "bitcoinsign.bank.building.fill")
+                .resizable()
+                .frame(width: sizes.width / 10, height: sizes.height / 20)
+                .foregroundStyle(.yellow)
+                
+        }
     }
+    private var textColor: Color {
+            colorScheme == .dark ? .black : .white
+        }
 }
 
-#Preview {
-    HeaderView() 
-}
