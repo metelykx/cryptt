9e8c4557cd89e94d82c9244404e235f7d0eaa607
metelykx <ivaschenkoodenys@gmail.com>
2025-08-11 12:28:01 +0300

    ZStack

--- Cryptt/LifeCycle/LifeActiveView.swift
+++ Cryptt/LifeCycle/LifeActiveView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct LifeActiveView: View {
     var body: some View {
-        Text("LifeActiveView")
+        ZStack {
+            Text("LifeActiveView")
+        }
     }
 }
 
