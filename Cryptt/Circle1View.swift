03f5300e8811b1b48bc0afff10b35ddc0eae690d
metelykx <ivaschenkoodenys@gmail.com>
2025-08-11 11:04:18 +0300

    struct as Circle1

--- Cryptt/Circle1View.swift
+++ Cryptt/Circle1View.swift
@@ -5,4 +5,18 @@
 //  Created by Denis Ivaschenko on 11.08.2025.
 //
 
-import SwiftUI 
+import SwiftUI
+ 
+struct Circle1View: View {
+    let size: CGSize
+    var body: some View {
+        Circle()
+            .fill(Color.gray.opacity(0.2))
+            .frame(width: size.width / 2, height: size.height / 2)
+            .rotationEffect(.init(degrees: 45))
+            .offset(
+                x: -size.width / 4,
+                y: -size.height / 4
+            )
+    }
+}
