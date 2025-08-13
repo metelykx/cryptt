041cfe9c4f2527a04159733febb82a24afc97131
metelykx <ivaschenkoodenys@gmail.com>
2025-08-13 10:10:41 +0300

    struct for get info

--- Cryptt/Model.swift
+++ Cryptt/Model.swift
@@ -5,4 +5,22 @@
 //  Created by Denis Ivaschenko on 13.08.2025.
 //
 
-import Foundation 
+import Foundation
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let data: [Datum]
+    let timestamp: Int
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+    let id, rank, symbol, name: String
+    let supply: String
+    let maxSupply: String?
+    let marketCapUsd, volumeUsd24Hr, priceUsd, changePercent24Hr: String
+    let vwap24Hr: String?
+    let explorer: String?
+    let tokens: [String: [String]]
+}
+
